accc371de0768e560118f863182e9e2be7e77a3b
T1 Pagos 4 <t1pagos4@192.168.1.10>
2023-10-10 13:55:42 -0600

    test commit

---
+++ src/stories/Card.tsx
@@ -0,0 +1,8 @@
+import CustomCard from "../components/CustomCard";
+import "./button.css";
+/**
+ * Primary UI component for user interaction
+ */
+export const Card = () => {
+  return <CustomCard />;
+};
